025139a8ca31db8343e52edbd526b33e0066a4ff
Chandler Lucius <clucius@chandlerlucius.com>
2020-03-08 12:32:23 -0500

    Cleaned up App.js and extrapolated html/js

--- client/src/App.js
+++ client/src/App.js
@@ -3,48 +3,18 @@ import './App.css';
 
 function App() {
   return (
-    <form method="POST" action="/location/add" className="container">
+    <div className="container">
       <h1>What's For Lunch?</h1>
-      <label htmlFor="name">Location</label>
-      <input name="name" id="name" type="text"></input>
-      <br/>
-      <button>Add</button>
-    </form>
+      <div>
+        <input id="restaurant-search" placeholder="Search for restaurant by name" type="text" />
+        <input id="food-search" placeholder="Search for restaurant by food type" type="search" />
+        <button id="food-search-button">Search</button>
+      </div>
+
+      <div className="map-container"></div>
+      <div className="details-container"></div>
+    </div>
   );
 }
-
-document.addEventListener('DOMContentLoaded', function() {
-  document.querySelectorAll('form').forEach(function(form) {
-    form.addEventListener('submit', function(e) {
-      e.preventDefault();
-      let action = form.action;
-      if(action.indexOf('localhost') !== -1) {
-        action = action.replace('3000', '8080');
-      }
-      const xhr = new XMLHttpRequest();
-      xhr.open(form.method, action);
-      xhr.onload = function() {
-        if(xhr.readyState === 4) {
-          if(xhr.status === 200) {
-            console.log("Added!");
-          } else {
-            const json = JSON.parse(xhr.responseText);
-            alert(json.body);
-          }
-        }
-      };
-      xhr.onerror = function() {
-        if(xhr.readyState === 4) {
-          if(xhr.status === 200) {
-            console.log("Failed!");
-          } else {
-            console.log("Failed!");
-          }
-        }
-      };
-      xhr.send(new FormData(form));
-    });
-  });
-});
 
 export default App;
