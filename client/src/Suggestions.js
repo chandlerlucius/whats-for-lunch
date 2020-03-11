40f6caeebe8de5938fea40ea19d510578818c6da
Chandler Lucius <clucius@chandlerlucius.com>
2020-03-11 15:17:19 -0500

    Created Suggestions component
    -Contains Suggestions already added from google maps results listing
    -Also contains anything included in a Restaurant Details component for each result

---
+++ client/src/Suggestions.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import RestaurantDetails from './RestaurantDetails'
+
+class Suggestions extends React.Component {
+    render() {
+      return (
+        <div>
+          {this.props.locations && this.props.locations.map((location, index) =>
+            <div key={index} className="flex">
+              <div className="votes-container">
+                <h3>▲</h3>
+                <h3>{location.votes}</h3>
+                <h3>▼</h3>
+              </div>
+              <div>
+                <h3>{location.name}</h3>
+                <RestaurantDetails place={location}/>
+              </div>
+            </div>
+          )}
+        </div>
+      )
+    }
+  }
+
+  export default Suggestions;
