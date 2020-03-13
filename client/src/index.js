c778c7c071483fff3c576a3347d1175254779534
Chandler Lucius <clucius@chandlerlucius.com>
2020-03-13 16:49:43 -0500

    Updated index react component
    -Now on render client must authenticate and use jwt
    -If no jwt in localStorage or other error then user is sent to Login component
    -If jwt found and valid then user gets sent to App component

--- client/src/index.js
+++ client/src/index.js
@@ -1,88 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Login from './Login';
 import App from './App';
-import Map from './Map';
-import Search from './Search';
-import Suggestions from './Suggestions';
-import './index.css';
 import * as serviceWorker from './serviceWorker';
-
-let url;
-let socket;
-let socketTimeout;
-
-ReactDOM.render(<App />, document.querySelector('#root'));
-
-ReactDOM.render(<Search />, document.querySelector('.search-container'));
-
-ReactDOM.render(<Map />, document.querySelector('.map-container'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
 
-if (window) {
-  window.addEventListener('DOMContentLoaded', function () {
-    let host = window.location.host;
-    host = host.replace('3000', '9000');
-    if (window.location.protocol === 'https:') {
-      url = `wss://${host}/locations`;
-    } else {
-      url = `ws://${host}/locations`;
+document.addEventListener('DOMContentLoaded', function (event) {
+  let href = window.location.href;
+  href = href.replace('3000', '9000');
+
+  const token = localStorage.getItem('token');
+  const xhr = new XMLHttpRequest();
+  xhr.open('POST', href + 'authenticate?token=' + token);
+  xhr.onload = function () {
+    if (xhr.readyState === 4) {
+      if (xhr.status === 200) {
+        ReactDOM.render(<App />, document.querySelector('#root'));
+      } else {
+        ReactDOM.render(<Login />, document.querySelector('#root'));
+      }
     }
-    start();
-  });
-}
-
-const clearSocketTimeout = function () {
-  if (socketTimeout) {
-    clearTimeout(socketTimeout);
   }
-}
-
-const setSocketTimeout = function () {
-  console.log('Socket closed. Attempting reconnect in 5 seconds.');
-  socketTimeout = setTimeout(function () {
-    start();
-  }, 5000);
-}
-
-const start = function () {
-  try {
-    socket = new WebSocket(url);
-    socket.onopen = function () {
-      clearSocketTimeout();
-    };
-
-    socket.onclose = function () {
-      setSocketTimeout();
-    };
-
-    socket.onerror = function () {
-      socket.close();
-    };
-
-    socket.onmessage = function (event) {
-      const locations = JSON.parse(event.data);
-      ReactDOM.render(<Suggestions locations={locations} />, document.querySelector('.suggestions-container'));
-    }
-  } catch (error) {
-    //Ignore error on purpose
+  xhr.onerror = function () {
+    ReactDOM.render(<Login />, document.querySelector('#root'));
   }
-};
-
-const convertFormSubmission = function (form) {
-  form.addEventListener('submit', function (e) {
-    e.preventDefault();
-    const object = {};
-    const formData = new FormData(form);
-    formData.forEach(function (value, key) {
-      object[key] = value;
-    });
-    const json = JSON.stringify(object);
-    socket.send(json);
-  });
-};
-
-export default convertFormSubmission;
+  xhr.send();
+});
