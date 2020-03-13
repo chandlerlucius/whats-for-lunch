6266d8ebee81d577d1c5d8e8ca664b20e3bb626a
Chandler Lucius <clucius@chandlerlucius.com>
2020-03-13 16:58:25 -0500

    Updated websocket connection details
    -Moved mongo and database info to main
    -Added authentication to websocket connection
    -Updated use of ctx throughout websocket
    -Fixed functions inserting db entries to return only one result and one error
    -Moved Message struct to main

--- server/websocket.go
+++ server/websocket.go
@@ -11,12 +11,8 @@ import (
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
-	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-var mongoClient, err = mongo.NewClient(options.Client().ApplyURI("mongodb://localhost:27017"))
-var database = "lunch"
-var collection = "location"
 var clients = make(map[*websocket.Conn]bool)
 
 var upgrader = websocket.Upgrader{
@@ -32,6 +28,15 @@ func locationsWebsocketHandler(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		log.Print(err)
 	}
+
+	status, err1 := authenticate(w, r)
+	if err1 != nil || status != http.StatusOK {
+		log.Print(err1)
+		message := websocket.FormatCloseMessage(websocket.CloseInternalServerErr, "Unauthorized access!")
+		c.WriteMessage(websocket.CloseMessage, message)
+		return
+	}
+
 	clients[c] = true
 	getLocations(c)
 	handleConnection(c)
@@ -65,38 +70,33 @@ func getLocations(c *websocket.Conn) {
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 
-	err1 := mongoClient.Connect(ctx)
-	if err1 != nil {
-		log.Print(err1)
-	}
-
-	collection := mongoClient.Database(database).Collection(collection)
-	cur, err2 := collection.Find(context.Background(), bson.D{})
-	if err2 != nil {
-		log.Print(err2)
+	collection := mongoClient.Database(database).Collection("location")
+	cur, err := collection.Find(ctx, bson.D{})
+	if err != nil {
+		log.Print(err)
 	}
 
 	var results []*bson.M
-	defer cur.Close(context.Background())
-	for cur.Next(context.Background()) {
+	defer cur.Close(ctx)
+	for cur.Next(ctx) {
 		var result bson.M
-		err3 := cur.Decode(&result)
-		if err3 != nil {
-			log.Print(err3)
+		err1 := cur.Decode(&result)
+		if err1 != nil {
+			log.Print(err1)
 		}
 		results = append(results, &result)
 	}
 
-	err4 := c.WriteJSON(results)
-	if err4 != nil {
-		log.Print(err4)
+	err2 := c.WriteJSON(results)
+	if err2 != nil {
+		log.Print(err2)
 	}
 }
 
 func addLocation(c *websocket.Conn, bytes []byte) {
 	location := &Location{}
 	err := json.Unmarshal(bytes, location)
-	res, err1, err2 := insertOneIntoDatabase(location, database, collection)
+	res, err1 := insertLocationIntoDatabase(location, "location")
 
 	if err != nil {
 		info := "Sorry, we encountered an error our end. It has been logged and will be fixed"
@@ -106,36 +106,19 @@ func addLocation(c *websocket.Conn, bytes []byte) {
 		info := "Sorry, we encountered an error our end. It has been logged and will be fixed"
 		log.Print(info, " : ", err1)
 	}
-	if err2 != nil {
-		info := "Sorry, we encountered an error our end. It has been logged and will be fixed"
-		log.Print(info, " : ", err2)
-	}
 	if res != nil {
 		info := "Added to DB succesffully with name: " + location.Name + " | id:" + res.InsertedID.(primitive.ObjectID).String()
 		log.Print(info)
 	}
 }
 
-func insertOneIntoDatabase(location *Location, database string, collection string) (res *mongo.InsertOneResult, err error, err1 error) {
+func insertLocationIntoDatabase(location *Location, collection string) (*mongo.InsertOneResult, error) {
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 
-	err2 := mongoClient.Connect(ctx)
 	mongoCollection := mongoClient.Database(database).Collection(collection)
-	res1, err3 := mongoCollection.InsertOne(ctx, location)
-	return res1, err2, err3
-}
-
-func sendAndLogError(err error, w http.ResponseWriter) {
-	if err != nil {
-		log.Print(err)
-		info := "Sorry, we encountered an error our end. It has been logged and will be fixed!"
-		message := Message{"Failure", info}
-		err1 := json.NewEncoder(w).Encode(message)
-		if err1 != nil {
-			log.Print(err1)
-		}
-	}
+	res, err := mongoCollection.InsertOne(ctx, location)
+	return res, err
 }
 
 // Location is used to identify the values of a restaurant
@@ -147,10 +130,4 @@ type Location struct {
 	Website          string `json:"website"`
 	URL              string `json:"url"`
 	Votes            string `json:"votes"`
-}
-
-// Message is used to send a title and body as error message to client
-type Message struct {
-	Title string `json:"title"`
-	Body  string `json:"body"`
 }
