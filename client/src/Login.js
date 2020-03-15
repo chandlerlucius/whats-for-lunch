692f19d6a173f73cc0c931b2c35c93097ae5b7a3
Chandler Lucius <clucius@chandlerlucius.com>
2020-03-15 12:46:53 -0500

    Updated login component
    -Added message value to state for showing errors to client
    -Updated html to contain nice login form
    -Added labels and spans for fly aways
    -Added spans for helper text
    -Used message to determine color of error message
    -Updated DOMContentLoaded listener to use react functions through onSubmit listener instead

--- client/src/Login.js
+++ client/src/Login.js
@@ -1,40 +1,74 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
-import './App.css';
 
 class Login extends React.Component {
-  componentDidMount() {
-    const form = document.querySelector('.login-container form');
-    form.addEventListener('submit', function (e) {
-      e.preventDefault();
-      let action = form.action.replace('3000', '9000');
+  constructor(props) {
+    super(props);
+    localStorage.removeItem("token");
+    this.state = { message: props.message }
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
 
-      const xhr = new XMLHttpRequest();
-      xhr.open(form.method, action);
-      xhr.onload = function () {
-        if (xhr.readyState === 4) {
-          if (xhr.status === 200) {
-            const json = JSON.parse(xhr.responseText);
-            localStorage.setItem("token", json.token)
-            ReactDOM.render(<App />, document.querySelector('#root'));
-          }
+  handleSubmit(event) {
+    const react = this;
+    event.preventDefault();
+    const form = event.target;
+    const method = form.method;
+    const action = form.action.replace('3000', '9000');
+
+    const xhr = new XMLHttpRequest();
+    xhr.open(method, action);
+    xhr.onload = function () {
+      if (xhr.readyState === 4) {
+        if (xhr.status === 200) {
+          const json = JSON.parse(xhr.responseText);
+          localStorage.setItem("token", json.token)
+          ReactDOM.render(<App />, document.querySelector('.root'));
+        } else {
+          const json = JSON.parse(xhr.responseText);
+          document.querySelectorAll(".message").forEach(function (element) {
+            react.setState({
+              message : json.body
+            });
+          });
         }
       }
-      xhr.send(new FormData(form));
-    });
+    }
+    xhr.onerror = function () {
+      react.setState({
+        message : 'Error connecting to server. Try again later.'
+      });
+    }
+    xhr.send(new FormData(form));
+  }
+  
+  color() {
+    if (this.state.message === 'Enter username and password to begin.') {
+      return 'var(--secondary-color)';
+    }
+    return 'var(--failure-color)';
   }
 
   render() {
     return (
-      <div className="login-container container">
+      <form method="POST" action="/login" className="login-form" onSubmit={this.handleSubmit}>
         <h1>What's For Lunch?</h1>
-        <form method="POST" action="/login">
-          <input name="username" type="text" required={true}></input>
-          <input name="password" type="password" required={true}></input>
-          <button>Login / Signup</button>
-        </form>
-      </div>
+        <div className="input-container">
+          <input name="username" type="text" placeholder=" " required={true} minLength="3" autoFocus={true}></input>
+          <label htmlFor="username">Username</label>
+          <span>Must have 3 or more characters</span>
+        </div>
+        <div className="input-container">
+          <input name="password" type="password" placeholder=" " required={true} minLength="3"></input>
+          <label htmlFor="password">Password</label>
+          <span>Must have 3 or more characters</span>
+        </div>
+        <span className="message" style={{ color: this.color() }}>{this.state.message}</span>
+        <hr />
+        <button type="reset">Reset</button>
+        <button type="submit">Login / Signup</button>
+      </form>
     )
   }
 }
