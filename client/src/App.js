5e5903a3efff74d69d1d48b0c201bb6246e423e3
Chandler Lucius <clucius@chandlerlucius.com>
2020-03-17 23:14:44 -0500

    Updated main page of app
    -Extrapolated host and url variables
    -Updated ws onmessage to handle errors, chats, and locations
    -Added top nav, letf popout, right popout, center, and toast elements
    -Added methods to handle opening and closing left and right popouts with toggles
    -Added chat window on left and toast window on bottom
    -Updated json submit method to set type before sending request

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,23 @@
 import React from 'react';
-import ReactDOM from 'react-dom'
-import Map from './Map'
-import Login from './Login'
-import Search from './Search'
-import Suggestions from './Suggestions'
+import ReactDOM from 'react-dom';
 import './index.css';
+import Map from './Map';
+import Chat from './Chat';
+import Toast from './Toast'
+import Search from './Search';
+import Suggestions from './Suggestions';
 
 let url;
 let socket;
 let socketTimeout;
+let host = window.location.host;
+host = host.replace('3000', '9000');
+if (window.location.protocol === 'https:') {
+  url = `wss://${host}/ws`;
+} else {
+  url = `ws://${host}/ws`;
+}
+
 class App extends React.Component {
   componentDidMount() {
     ReactDOM.render(<Search />, document.querySelector('.search-container'));
@@ -37,11 +46,7 @@ class App extends React.Component {
         };
 
         socket.onclose = function (event) {
-          if (event.code === 4001) {
-            ReactDOM.render(<Login />, document.querySelector('#root'));
-          } else {
-            setSocketTimeout();
-          }
+          setSocketTimeout();
         };
 
         socket.onerror = function () {
@@ -49,40 +54,75 @@ class App extends React.Component {
         };
 
         socket.onmessage = function (event) {
-          const locations = JSON.parse(event.data);
-          ReactDOM.render(<Suggestions locations={locations} />, document.querySelector('.suggestions-container'));
+          const json = JSON.parse(event.data);
+          if (json.type === 'error') {
+            ReactDOM.render(<Toast message={json.body} />, document.querySelector('.toast-container'));
+            document.querySelector('.toast-container').style.marginBottom = '0';
+          } else if (json.type === 'chat') {
+            ReactDOM.render(<Chat messages={json.body} />, document.querySelector('.chat-container'));
+          } else if (json.type === 'location') {
+            ReactDOM.render(<Suggestions locations={json.body} />, document.querySelector('.suggestions-container'));
+          }
         }
       } catch (error) {
         //Ignore error on purpose
       }
     };
-
-    let host = window.location.host;
-    host = host.replace('3000', '9000');
-    if (window.location.protocol === 'https:') {
-      url = `wss://${host}/locations`;
-    } else {
-      url = `ws://${host}/locations`;
-    }
     start();
+
+    convertFormSubmitToJsonSubmit(document.querySelector('.chat-form'));
+  }
+
+  toggleLeftMenu(event) {
+    const menu = document.querySelector('.left');
+    if (menu.classList.contains('left-menu-open')) {
+      event.target.innerHTML = '💬';
+      menu.classList.remove('left-menu-open');
+    } else {
+      event.target.innerHTML = '🡠';
+      menu.classList.add('left-menu-open');
+    }
+  }
+
+  toggleRightMenu() {
+    const menu = document.querySelector('.right');
+    if (menu.classList.contains('right-menu-open')) {
+      menu.classList.remove('right-menu-open');
+    } else {
+      menu.classList.add('right-menu-open');
+    }
   }
 
   render() {
-    return (
-      <div className="container">
-        <h1>What's For Lunch?</h1>
+    return [
+      <nav key="nav">
+        <h2 className="toggle" onClick={this.toggleLeftMenu}>💬</h2>
+        <h2>What's For Lunch?</h2>
+        <h2 className="toggle" onClick={this.toggleRightMenu}>☰</h2>
+      </nav>,
+      <div key="left" className="left">
+        {/* <h2 className="close toggle" onClick={this.toggleLeftMenu}>✕</h2> */}
+        <div className="chat-container"></div>
+        <form className="chat-form" action="chat">
+          <textarea name="message" className="chat"></textarea>
+          <button type="submit">Send</button>
+        </form>
+      </div>,
+      <div key="center" className="center">
         <div className="suggestions-container"></div>
         <div className="search-container"></div>
         <div className="map-container"></div>
-        <div className="details-container">
-          <span className="toggle">☰</span>
-        </div>
-      </div>
-    )
+      </div>,
+      <div key="right" className="right">
+        <h2 className="close toggle" onClick={this.toggleRightMenu}>✕</h2>
+        <div className="details-container"></div>
+      </div>,
+      <div className="toast-container"></div>
+    ]
   }
 }
 
-export const convertFormSubmissionToJSON = function (form) {
+export const convertFormSubmitToJsonSubmit = function (form) {
   form.addEventListener('submit', function (e) {
     e.preventDefault();
     const object = {};
@@ -90,9 +130,10 @@ export const convertFormSubmissionToJSON = function (form) {
     formData.forEach(function (value, key) {
       object[key] = value;
     });
+    object['type'] = form.action.split('/').pop();
     const json = JSON.stringify(object);
     socket.send(json);
   });
-};
+}
 
 export default App;
