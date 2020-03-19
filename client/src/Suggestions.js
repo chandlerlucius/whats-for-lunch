aea44fc29e5a8163d39139331c0c7a3f02f3894f
Chandler Lucius <clucius@chandlerlucius.com>
2020-03-19 18:41:53 -0500

    Added client side voting functionality

--- client/src/Suggestions.js
+++ client/src/Suggestions.js
@@ -1,16 +1,29 @@
 import React from 'react'
 import RestaurantDetails from './RestaurantDetails'
+import { sendWebsocketMessage } from './App'
 
 class Suggestions extends React.Component {
+
+  handleVote(event) {
+    const map = {};
+    if (event.target.innerHTML === '▲') {
+      map['vote'] = 'up';
+    } else if (event.target.innerHTML === '▼') {
+      map['vote'] = 'down';
+    }
+    map['type'] = 'votes';
+    sendWebsocketMessage(map);
+  }
+
   render() {
     return (
       <div>
         {this.props.locations && this.props.locations.map((location, index) =>
           <div key={index} className="votes-container">
             <div>
-              <h3>▲</h3>
+              <h3 onClick={this.handleVote}>▲</h3>
               <h3>{location.votes}</h3>
-              <h3>▼</h3>
+              <h3 onClick={this.handleVote}>▼</h3>
             </div>
             <div>
               <h3>{location.name}</h3>
