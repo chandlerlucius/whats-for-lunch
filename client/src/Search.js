8eb496a7208f091b6f7b92a84c5f3f2ac158a0fa
Chandler Lucius <clucius@chandlerlucius.com>
2020-03-20 18:59:50 -0500

    Added keys to elements in Search component

--- client/src/Search.js
+++ client/src/Search.js
@@ -3,9 +3,9 @@ import React from 'react'
 class Search extends React.Component {
   render() {
     return [
-      <input id="restaurant-search" placeholder="Search for restaurant by name" type="text" />,
-      <input id="food-search" placeholder="Search for restaurant by food type" type="search" />,
-      <button id="food-search-button">Search</button>
+      <input key="restaurant-seach" id="restaurant-search" placeholder="Search for restaurant by name" type="text" />,
+      <input key="food-search" id="food-search" placeholder="Search for restaurant by food type" type="search" />,
+      <button key="food-search-button" id="food-search-button">Search</button>
     ]
   }
 }
