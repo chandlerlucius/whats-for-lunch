817300aee7300d27fd34fa1ca736a7df3bf839f3
Chandler Lucius <clucius@chandlerlucius.com>
2020-03-22 15:38:47 -0500

    Updated suggestion component
    -Added checkmark for first in list (highest voted)
    -Updated getStyle method to getArrowStyle method for making it blue if client voted
    -Added new method to get vote count border style

--- client/src/Suggestions.js
+++ client/src/Suggestions.js
@@ -22,13 +22,20 @@ class Suggestions extends React.Component {
     return title;
   }
 
-  getStyle(location, value) {
+  getArrowStyle(location, value) {
     let style = {};
-    if (location.voted === 1 && value === 1) {
-      style = { color: 'var(--success-color)' };
+    if (location.voted === value) {
+      style = { color: 'var(--primary-color)' };
     }
-    if (location.voted === -1 && value === -1) {
-      style = { color: 'var(--failure-color)' };
+    return style;
+  }
+
+  getCountStyle(location) {
+    let style = {};
+    if (location.vote_count > 0) {
+      style = { 'border-color': 'var(--success-color)', 'color': 'var(--success-color)' };
+    } else if (location.vote_count < 0) {
+      style = { 'border-color': 'var(--failure-color)', 'color': 'var(--failure-color)' };
     }
     return style;
   }
@@ -38,11 +45,16 @@ class Suggestions extends React.Component {
       this.props.locations && this.props.locations.map((location, index) =>
         <form key={index} action="vote" className="vote-form">
           <div>
-            <h2 title="Click to add or remove up vote" onClick={this.handleVote} value="1" style={this.getStyle(location, 1)}>▲</h2>
-            <div className="vote-container">
+            {index === 0 ? <h2 className="vote-checkmark">✔</h2> : <h2> </h2>}
+            <h2 title="Click to add or remove up vote" onClick={this.handleVote} value="1" style={this.getArrowStyle(location, 1)}>
+              ▲
+            </h2>
+            <div className="vote-container" style={this.getCountStyle(location)}>
               <h3 title={this.getTitle(location)}>{location.vote_count}</h3>
             </div>
-            <h2 title="Click to add or remove down vote" onClick={this.handleVote} value="-1" style={this.getStyle(location, -1)}>▼</h2>
+            <h2 title="Click to add or remove down vote" onClick={this.handleVote} value="-1" style={this.getArrowStyle(location, -1)}>
+              ▼
+            </h2>
           </div>
           <div>
             <h3>{location.name}</h3>
