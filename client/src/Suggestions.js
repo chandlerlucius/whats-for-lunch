1e54a2da7eabade16344e4d1e00e0e06199fd385
Chandler Lucius <clucius@chandlerlucius.com>
2020-03-23 11:27:34 -0500

    Updated border-color to borderColor and handleVote() to submitForm()

--- client/src/Suggestions.js
+++ client/src/Suggestions.js
@@ -3,7 +3,7 @@ import { submitFormAsJson, formatDate } from './App'
 
 class Suggestions extends React.Component {
 
-  handleVote(event) {
+  submitForm(event) {
     const value = event.target;
     const form = event.target.closest('form');
     const input = form.querySelector('[name="value"]');
@@ -33,9 +33,9 @@ class Suggestions extends React.Component {
   getCountStyle(location) {
     let style = {};
     if (location.vote_count > 0) {
-      style = { 'border-color': 'var(--success-color)', 'color': 'var(--success-color)' };
+      style = { borderColor: 'var(--success-color)', color: 'var(--success-color)' };
     } else if (location.vote_count < 0) {
-      style = { 'border-color': 'var(--failure-color)', 'color': 'var(--failure-color)' };
+      style = { borderColor: 'var(--failure-color)', color: 'var(--failure-color)' };
     }
     return style;
   }
@@ -46,13 +46,13 @@ class Suggestions extends React.Component {
         <form key={index} action="vote" className="vote-form">
           <div>
             {index === 0 ? <h2 className="vote-checkmark">✔</h2> : <h2> </h2>}
-            <h2 title="Click to add or remove up vote" onClick={this.handleVote} value="1" style={this.getArrowStyle(location, 1)}>
+            <h2 title="Click to add or remove up vote" onClick={this.submitForm} value="1" style={this.getArrowStyle(location, 1)}>
               ▲
             </h2>
             <div className="vote-container" style={this.getCountStyle(location)}>
               <h3 title={this.getTitle(location)}>{location.vote_count}</h3>
             </div>
-            <h2 title="Click to add or remove down vote" onClick={this.handleVote} value="-1" style={this.getArrowStyle(location, -1)}>
+            <h2 title="Click to add or remove down vote" onClick={this.submitForm} value="-1" style={this.getArrowStyle(location, -1)}>
               ▼
             </h2>
           </div>
