cf0c60c519b9d4ca2503cb6db161a99f6b62eb14
Chandler Lucius <clucius@chandlerlucius.com>
2020-03-28 14:01:31 -0500

    Changed user to user_name, updated vote values to up and down, added titles, and added logic for remove location button

--- client/src/Suggestions.js
+++ client/src/Suggestions.js
@@ -14,10 +14,10 @@ class Suggestions extends React.Component {
   getTitle(location) {
     let title = "";
     location.up_votes.forEach(function (vote) {
-      title += '▲ ' + vote.user + ' ' + formatDate(vote.date) + '\n';
+      title += '▲ ' + vote.user_name + ' ' + formatDate(vote.date) + '\n';
     });
     location.down_votes.forEach(function (vote) {
-      title += '▼ ' + vote.user + ' ' + formatDate(vote.date) + '\n';
+      title += '▼ ' + vote.user_name + ' ' + formatDate(vote.date) + '\n';
     });
     return title;
   }
@@ -45,19 +45,22 @@ class Suggestions extends React.Component {
       this.props.locations && this.props.locations.map((location, index) =>
         <form key={index} action="vote" className="vote-form">
           <div>
-            {index === 0 ? <h2 className="vote-checkmark">✔</h2> : <h2> </h2>}
-            <h2 title="Click to add or remove up vote" onClick={this.submitForm} value="1" style={this.getArrowStyle(location, 1)}>
+            {index === 0 ? <h2 className="vote-checkmark" title="Lunch is here!">✔</h2> : <h2> </h2>}
+            <h2 title="Click to add or remove up vote" onClick={this.submitForm} value="up" style={this.getArrowStyle(location, "up")}>
               ▲
             </h2>
             <div className="vote-container" style={this.getCountStyle(location)}>
               <h3 title={this.getTitle(location)}>{location.vote_count}</h3>
             </div>
-            <h2 title="Click to add or remove down vote" onClick={this.submitForm} value="-1" style={this.getArrowStyle(location, -1)}>
+            <h2 title="Click to add or remove down vote" onClick={this.submitForm} value="down" style={this.getArrowStyle(location, "down")}>
               ▼
             </h2>
           </div>
           <div>
             <h3>{location.name}</h3>
+            {location.added ?
+              <h2 className="vote-remove" title="Remove location" onClick={this.submitForm} value="remove">✕</h2>
+              : ""}
           </div>
           <input type="hidden" name="location" value={location.name}></input>
           <input type="hidden" name="value"></input>
