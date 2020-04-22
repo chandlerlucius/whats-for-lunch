1a6ddedc5a475b8482273fb5609416000fad14a5
Chandler Lucius <clucius@chandlerlucius.com>
2020-04-21 21:32:42 -0500

    Removed unused imports

--- client/src/Users.js
+++ client/src/Users.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { formatDate, submitFormAsJson, submitFormWithEvent } from './App';
+import { formatDate, submitFormAsJson } from './App';
 
 class Users extends React.Component {
 
